6b41f2499c1095aae74f458a6dc585a68d01e3b3
Edwin Webb <edwin.webb@gmail.com>
2022-05-10 16:29:19 -0700

    add MD layout

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -18,6 +18,7 @@ const LCanvas = dynamic(() => import('@/components/layout/canvas'), {
 // Split components into DOM or R3F by prop flags
 const Balance = ({ child }) => {
   const [r3f, dom] = partition(child, (c) => c.props.r3f === true)
+  const hasCanvas = r3f.length > 0
 
   return (
     <>
@@ -26,10 +27,10 @@ const Balance = ({ child }) => {
         <Dom>{dom}</Dom>
         {r3f.length > 0 && <LCanvas>{r3f}</LCanvas>}
       </div> */}
-      <div className='bg-red-100 flex flex-col flex-wrap h-screen'>
-        <div className='bg-yellow-100 h-12'>menu</div>
-        <div className='bg-green-100 h-96'>dom</div>
-        {r3f.length > 0 && <div className='bg-blue-100 flex-1'>c</div>}
+      <div className='bg-red-100 flex flex-col flex-wrap h-screen md:flex-row'>
+        <div className='bg-yellow-100 h-12 w-screen'>menu</div>
+        <div className='bg-green-100 h-96 md:h-full md:w-80'>dom</div>
+        {hasCanvas && <div className='bg-blue-100 flex-1'>c</div>}
       </div>
     </>
   )
