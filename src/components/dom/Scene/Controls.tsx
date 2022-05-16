fe5199a273b7dd26261ee033a023e5b117733760
Edwin Webb <edwin.webb@gmail.com>
2022-05-16 14:53:41 -0700

    Add fork and save handlers

--- src/components/dom/Scene/Controls.tsx
+++ src/components/dom/Scene/Controls.tsx
@@ -1,23 +1,30 @@
 interface ControlButtonProps {
   label: string;
   iconCode: string;
+  onClick: () => void
 }
 
 const ControlButton = (props: ControlButtonProps) => {
-  const { label, iconCode } = props
-  return (<span>
+  const { label, iconCode, onClick } = props
+  return (<span onClick={() => { onClick() }}>
     <span className="font-icon">{iconCode}</span>
     <span className="hidden invisible">{label}</span>
   </span>)
 }
 
-const Controls = () => {
+interface SceneControlProps {
+  saveHandler: () => void;
+  forkHandler: () => void;
+}
+
+const Controls = (props: SceneControlProps) => {
+  const { saveHandler, forkHandler } = props
   return (<div className="bg-blackpink-900 text-white h-18 py-1">
     <span className="font-icon">account_tree</span>
     <span>SCENE_ID</span>
-    <ControlButton label="Save Scene" iconCode="save" />
-    <ControlButton label="Fork Scene" iconCode="fork_right" />
-    <ControlButton label="Add Item" iconCode="add" />
+    <ControlButton label="Save Scene" iconCode="save" onClick={() => saveHandler()} />
+    <ControlButton label="Fork Scene" iconCode="fork_right" onClick={() => forkHandler()} />
+    <ControlButton label="Add Item" iconCode="add" onClick={() => console.log('add item to scene')} />
   </div>)
 }
 
--- src/pages/s/[sid].tsx
+++ src/pages/s/[sid].tsx
@@ -4,6 +4,11 @@ import dynamic from 'next/dynamic'
 import SceneControls from '@/components/dom/Scene/Controls'
 import SceneGraph from '@/components/dom/Scene/Graph'
 import SceneProperties from '@/components/dom/Scene/Properties'
+import { firestore } from '@/firebase/clientApp';
+import { doc, getDoc, setDoc, addDoc, collection, Timestamp } from "@firebase/firestore";
+import { useEffect, useState } from 'react'
+import useStore from "@/helpers/store"
+
 
 // Dynamic import is used to prevent a payload when the website start that will include threejs r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
@@ -13,34 +18,58 @@ const VREditor = dynamic(() => import('@/components/canvas/Editor'), {
   ssr: false,
 })
 
-const SceneEditor = () => {
-  return (
-    <div className='md:flex md:flex-col md:h-full md:border-r-2 md:border-r-blackpink-800'>
-      <SceneControls />
-      <SceneGraph />
-      <SceneProperties />
-    </div>
-
-  )
-}
-
-// canvas components goes here
-const R3F = () => {
-  return (
-    <>
-      <VREditor />
-    </>
-  )
-}
-
 const Page = () => {
   const router = useRouter()
-  const { pid } = router.query
+  const { sid } = router.query
+  const [isLoading, setLoading] = useState<boolean>(true)
+  const [hasError, setError] = useState<boolean>(false)
+  const scene = useStore(state => state.scene)
+  const getScene = async (id: string) => {
+    const docRef = doc(firestore, "scenes", id)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) {
+      console.log(docSnap.data())
+    } else {
+      setError(true)
+    }
+
+    setLoading(false)
+  }
+  const forkScene = async () => {
+    console.log(scene);
+    return
+    // todo - change URL
+  }
+  const saveScene = async () => {
+    if (sid) {
+      await setDoc(doc(firestore, 'scenes', sid.toString()), {
+        updated: Timestamp.fromDate(new Date()),
+        scene
+      })
+    } else {
+      setError(true)
+    }
+  }
+
+  useEffect(() => {
+    if (sid) {
+      getScene(sid.toString())
+    } else {
+      setError(true)
+    }
+  })
+
+  // console.log(docSnap, pid)
   return (
     <>
-      <SceneEditor />
+      <div className='md:flex md:flex-col md:h-full md:border-r-2 md:border-r-blackpink-800'>
+        <SceneControls saveHandler={() => { saveScene() }} forkHandler={() => { forkScene() }} />
+        <SceneGraph />
+        <SceneProperties />
+      </div>
       {/* @ts-ignore */}
-      <R3F r3f />
+      <VREditor r3f loading={isLoading} error={hasError} />
     </>
   )
 }
