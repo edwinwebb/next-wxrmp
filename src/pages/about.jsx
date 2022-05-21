192ce33b9ffb8724f4312c01e6db3da25eda976f
Edwin Webb <edwin.webb@gmail.com>
2022-05-21 07:52:07 -0700

    Add some test to about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,23 @@
+import LOGO from '../../public/img/wxrmp-logo.png'
+import Image from 'next/image'
+
+
 const Page = () => {
   return (
     <>
       <div>
-        About - Hello World
+        <h2><Image src={LOGO} alt="WXRMP" height={36} width={36} /> WXRMP</h2>
+        <h3>Web XR Media Player</h3>
+        <h4>Experience your media in a new dimension</h4>
+      </div>
+      <div>
+        <h2>Free Forever</h2>
+        <p>Load in any media shared publicily on the internet</p>
+        <h2>Earn some coin</h2>
+        <p>Charge a fee to share your media on our platform</p>
+        <h2>Privacy First</h2>
+        <p>We won't share your media without your consent</p>
+        <p></p>
       </div>
     </>
   )
