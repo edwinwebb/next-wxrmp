00bbec41366871e2888eeaf0ee22b1d0e2ad3740
Edwin Webb <edwin.webb@gmail.com>
2022-05-22 21:19:37 -0700

    butotns spacing fix

--- src/components/dom/Menu/Menu.jsx
+++ src/components/dom/Menu/Menu.jsx
@@ -15,12 +15,12 @@ export default function Menu() {
       </div>
       <div className="pr-2">
         <Link href="/about">
-          <a className="invisible md:visible px-2 py-1 text-white/90 hover:bg-pink-200 rounded">About</a>
+          <a className="invisible md:visible px-2 py-1 mr-2 text-white/90 hover:bg-pink-200 rounded">About</a>
         </Link>
         <Link href="/contact">
-          <a className="invisible md:visible px-2 py-1 text-white/90 hover:bg-pink-200 rounded">Contact</a>
+          <a className="invisible md:visible px-2 py-1 mr-2 text-white/90 hover:bg-pink-200 rounded">Contact</a>
         </Link>
-        <Link href="/s/new"><button className="bg-pink-400 text-white rounded px-2 py-1">Create</button></Link>
+        <Link href="/s/new"><button className="bg-pink-600 text-white rounded px-2 py-1">Create</button></Link>
       </div>
     </nav>
   )
