fc6eaebffd2777ff4836a2f2d5dd7d2073227402
Edwin Webb <edwin.webb@gmail.com>
2022-06-02 20:43:20 -0700

    Rewrite About

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,30 +1,48 @@
 import LOGO from '../../public/img/wxrmp-logo.png'
 import Image from 'next/image'
+import { CashIcon, CurrencyDollarIcon, FastForwardIcon, TerminalIcon } from '@heroicons/react/solid'
 
 
 const Page = () => {
   return (
     <>
-      <div>
-        <h2><Image src={LOGO} alt="WXRMP" height={36} width={36} /> WXRMP</h2>
-        <h3>Web XR Media Player</h3>
-        <h4>Experience your media in a new dimension</h4>
+      <div className='container mx-auto md:w-[1024px]'>
+        <div className='w-60 md:w-80 h-60 md:h-80 my-4 md:my-8 mx-auto block'>
+          <Image src={LOGO} alt="WXRMP" />
+        </div>
+        <h2 className='text-center text-4xl font-black mb-4'>WXRMP</h2>
+        <h3 className='text-center text-4xl font-thin'>WebXR Media Player</h3>
+        <p className='text-md px-2 py-4 text-center font-thin'>
+          Experience your media in a new dimension.
+          Load in images and videos into an easily controled VR environment.
+          Play multiple videos and hundreds of images!
+        </p>
+        <hr className='m-2 my-4' />
+        <div className='grid grid-cols-4'>
+          <div className='text-center mx-4'>
+            <CashIcon className='w-32 h-32 mx-auto' />
+            <h2 className='text-xl font-bold my-2 text-center'>Free Forever</h2>
+            <p>Load in any media shared publicily on the internet or on your machine</p>
+          </div>
+          <div className='text-center mx-4'>
+            <CurrencyDollarIcon className='w-32 h-32 mx-auto' />
+            <h2 className='text-xl font-bold my-2 text-center'>Earn some Money</h2>
+            <p>Host your media on our platform to charge an access fee</p>
+          </div>
+          <div className='text-center mx-4'>
+            <TerminalIcon className='w-32 h-32 mx-auto' />
+            <h2 className='text-xl font-bold my-2 text-center'>Accessible</h2>
+            <p>Use the site on any heasdset which has a browser</p>
+          </div>
+          <div className='text-center mx-4'>
+            <FastForwardIcon className='w-32 h-32 mx-auto' />
+            <h2 className='text-xl font-bold my-2 text-center'>Perfomance</h2>
+            <p>Push your devices to the limit. Load in 100s of videos.</p>
+          </div>
+        </div>
+        <hr className='m-2 my-4' />
+
       </div>
-      <div>
-        <h2>Free Forever</h2>
-        <p>Load in any media shared publicily on the internet</p>
-        <h2>Earn some coin</h2>
-        <p>Charge a fee to share your media on our platform</p>
-        <h2>Privacy First</h2>
-        <p>You choose to make your media discoverable</p>
-        <h2>Performace</h2>
-        <p>We focus on performance, more media is more fun</p>
-        <h2>HTML Standards</h2>
-        <p>Access the site from any device on any platform</p>
-        <h2>UX</h2>
-        <p>Easy to use and consistent across devices</p>
-      </div>
-      <div></div>
     </>
   )
 }
