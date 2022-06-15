36afa2e8d823b2bbb9a47beb2f92ff0ac66bddc2
Edwin Webb <edwin.webb@gmail.com>
2022-06-15 11:00:52 -0700

    Update controls and add loading state

--- src/components/dom/Scene/Controls.tsx
+++ src/components/dom/Scene/Controls.tsx
@@ -1,5 +1,5 @@
 import useStore from '@/helpers/store'
-import { UploadIcon, DuplicateIcon, PlusSmIcon, PhotographIcon, FilmIcon, UserIcon } from '@heroicons/react/solid'
+import { UploadIcon, DuplicateIcon, PlusSmIcon, PhotographIcon, FilmIcon } from '@heroicons/react/solid'
 
 interface ControlButtonProps {
   children: React.ReactNode
@@ -13,33 +13,47 @@ const ControlButton = (props: ControlButtonProps) => {
   </button>)
 }
 
-interface SceneControlProps {
-  saveHandler: () => void;
-  forkHandler: () => void;
-  canSave: boolean;
+const LoadingButton = () => {
+  return <span className='rounded border-blackpink-800 mr-2 px-2 border-2 box-border '>
+    ...
+  </span>
 }
 
-const Controls = ({ saveHandler, forkHandler, canSave }: SceneControlProps) => {
+const Controls = () => {
   const addSceneItem = useStore(state => state.addSceneItem)
-  return (<div className="bg-blackpink-900 text-white h-18 py-2 flex flex-row-reverse border-blackpink-800 border-b-2">
-    <div>
-      {canSave && <ControlButton onClick={() => saveHandler()}>
-        <UploadIcon className='h-3 w-3 inline mr-1' />
-        <span className="text-xs">Save</span>
-      </ControlButton>}
-      <ControlButton onClick={() => forkHandler()} >
-        <DuplicateIcon className='h-3 w-3 inline mr-1' />
-        <span className="text-xs">Fork</span>
-      </ControlButton>
-      <ControlButton onClick={() => addSceneItem('image', 'image 1', [0, 1, -1 - Math.random()], [0, 0, 0], '404')} >
-        <PlusSmIcon className='h-3 w-3 inline mr-1' />
-        <PhotographIcon className='h-3 w-3 inline mr-1' />
-      </ControlButton>
-      <ControlButton onClick={() => addSceneItem('video', 'video 1', [0, 1, -1 - Math.random()], [0, 0, 0], '404')} >
-        <PlusSmIcon className='h-3 w-3 inline mr-1' />
-        <FilmIcon className='h-3 w-3 inline mr-1' />
-      </ControlButton>
-    </div>
+  const scene_uid = useStore(s => s.scene.author_uid)
+  const [uid, authed] = useStore(s => [s.author_uid, s.authorised])
+
+  return (<div className="bg-blackpink-900 text-white h-18 py-2 border-blackpink-800 border-b-2">
+    {
+      !authed
+        ? <div>
+          <LoadingButton />
+          <LoadingButton />
+          <LoadingButton />
+          <LoadingButton />
+        </div>
+        : <div>
+          {uid === scene_uid
+            && <ControlButton onClick={() => { }}>
+              <UploadIcon className='h-3 w-3 inline mr-1' />
+              <span className="text-xs">Save</span>
+            </ControlButton>
+          }
+          <ControlButton onClick={() => { }} >
+            <DuplicateIcon className='h-3 w-3 inline mr-1' />
+            <span className="text-xs">Fork</span>
+          </ControlButton>
+          <ControlButton onClick={() => addSceneItem('image', 'image 1', [0, 1, -1 - Math.random()], [0, 0, 0], '404')} >
+            <PlusSmIcon className='h-3 w-3 inline mr-1' />
+            <PhotographIcon className='h-3 w-3 inline mr-1' />
+          </ControlButton>
+          <ControlButton onClick={() => addSceneItem('video', 'video 1', [0, 1, -1 - Math.random()], [0, 0, 0], '404')} >
+            <PlusSmIcon className='h-3 w-3 inline mr-1' />
+            <FilmIcon className='h-3 w-3 inline mr-1' />
+          </ControlButton>
+        </div>
+    }
   </div>)
 }
 
